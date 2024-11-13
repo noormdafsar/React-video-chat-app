f1c8220898e23c058b740db7437fbb4e3f08f310
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-14 00:12:04 +0530

    router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+      </Routes>
+    </div>
   )
 }
 
